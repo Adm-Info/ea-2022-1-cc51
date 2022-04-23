ca8674460330238d0c6837598fe3df391c919647
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2022-04-23 15:27:59 -0500

    Pregunta a y b

--- code/Scripts.R
+++ code/Scripts.R
@@ -1,2 +1,7 @@
 #OPEN FILE
-hotel <- read.csv ("data/hotel_bookings_miss.csv", header = TRUE, sep = ",")
+hotel_data <- read.csv ("data/hotel_bookings_miss.csv", header = TRUE, sep = ",")
+#PREGUNTA A
+barplot(table(hotel_data$ï..hotel), main = "Reservas por tipo de hotel")
+#PREGUNTA B
+counts = table(hotel_data$is_canceled, hotel_data$arrival_date_year)
+barplot(counts, col=c("green","red"),legend = c("No Cancelado", "Cancelado"), main = "")
