00c251a262886fdacf11a74d37d18ca699e3ee5a
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2022-04-23 15:43:04 -0500

    Update Scripts.R

--- code/Scripts.R
+++ code/Scripts.R
@@ -1,7 +1,13 @@
-#OPEN FILE
+#CARGAR DATOS
 hotel_data <- read.csv ("data/hotel_bookings_miss.csv", header = TRUE, sep = ",")
+#PRE-PROCESAR DATOS
+
+
 #PREGUNTA A
-barplot(table(hotel_data$Ã¯..hotel), main = "Reservas por tipo de hotel")
+barplot(table(hotel_data$ï..hotel), main = "Reservas por tipo de hotel")
 #PREGUNTA B
 counts = table(hotel_data$is_canceled, hotel_data$arrival_date_year)
 barplot(counts, col=c("green","red"),legend = c("No Cancelado", "Cancelado"), main = "")
+#PREGUNTA C
+barplot(table(hotel_data$arrival_date_month), main = "Reservas por mes")
+
