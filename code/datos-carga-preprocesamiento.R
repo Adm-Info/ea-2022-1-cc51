556914223da17467b8d17b849b9330e03cf5de4c
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2022-04-24 04:04:25 -0500

    Identificación de datos en blancos o NA

--- code/datos-carga-preprocesamiento.R
+++ code/datos-carga-preprocesamiento.R
@@ -1,11 +1,12 @@
 #CARGAR DATOS
 hotel_data <- read.csv ("data/hotel_bookings_miss.csv", header = TRUE, sep = ",")
 
-#PRE-PROCESAR DATOS
 
+#INSPECCIONAR DATOS
 #verificamos que los datos estén acorde al tipo de dato
 str(hotel_data)
 
+#PRE-PROCESAR DATOS
 #las columnas is_canceled e is_repeated_guest serán convertidas en factor
 hotel_data$is_canceled <- as.factor(hotel_data$is_canceled)
 hotel_data$is_repeated_guest <- as.factor(hotel_data$is_repeated_guest)
@@ -18,3 +19,24 @@ hotel_data$reservation_status_date <- as.Date(hotel_data$reservation_status_date
 
 
 str(hotel_data)
+
+#Identificación de datos faltates
+datos_NA <- function(x){
+  sum = 0
+  for(i in 1:ncol(x))
+  {
+    cat("En la columna",colnames(x[i]),"total de valores NA:",colSums(is.na(x[i])),"\n")
+  }
+}
+
+datos_NA(hotel_data)
+
+#Identificamos datos en blanco
+en_blanco <- function(x){
+  sum = 0
+  for(i in 1:ncol(x))
+  {
+    cat("En la columna",colnames(x[i]),"total de valores en blanco:",colSums(x[i]==""),"\n")
+  }
+}
+en_blanco(hotel_data)
