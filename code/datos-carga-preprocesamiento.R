ad2b5aa6afe556e10638bef38d1973d03d2015ca
Coralain <73152381+Coralain10@users.noreply.github.com>
2022-05-05 09:51:39 -0500

    pre-procesamiento ordenado

--- code/datos-carga-preprocesamiento.R
+++ code/datos-carga-preprocesamiento.R
@@ -6,8 +6,38 @@ hotel_data <- read.csv ("data/hotel_bookings_miss.csv", header = TRUE, sep = ","
 #verificamos que los datos estén acorde al tipo de dato
 str(hotel_data)
 
-#PRE-PROCESAR DATOS
-#Identificación de datos faltates
+#las columnas is_canceled e is_repeated_guest serán convertidas en factor
+hotel_data$is_canceled <- as.factor(hotel_data$is_canceled)
+hotel_data$is_repeated_guest <- as.factor(hotel_data$is_repeated_guest)
+
+#la columna reservation_status_date será convertida a date
+#verificamos el formato de la fecha es dd/mm/yyyy o mm/dd/yyyy
+head(hotel_data$reservation_status_date,50)
+#comprobamos que es mm/dd/yyyy
+hotel_data$reservation_status_date <- as.Date(hotel_data$reservation_status_date, "%m/%d/%y")
+
+#Concatenaremos las columnas "arrival_date_..." para crear una sola con formato fecha
+#verificamos que los meses tienen formato con nombre del mes, por tanto los transformaremos en número
+
+
+
+hotel_data$arrival_date <- paste(hotel_data$arrival_date_year,
+                                  match(substr( hotel_data$arrival_date_month, 1, 3), month.abb),
+                                 hotel_data$arrival_date_day_of_month, sep="-")
+hotel_data$arrival_date <- as.Date(hotel_data$arrival_date)
+summary(hotel_data$arrival_date)
+
+
+#volvemos a comprobar la tabla
+str(hotel_data)
+
+
+
+# DUplicados
+#solo usaremos columnas no duplicadas
+hotel_datos<-unique(hotel_data)
+
+# Identificación de datos faltantes (NA).
 datos_NA <- function(x){
   sum = 0
   cat("Columnas : total de valores NA \n")
@@ -35,8 +65,6 @@ datos_NA(hotel_datos)
 #days_in_waiting_list : 7
 #arrival_date : 13 < es la cantidad de días NA y meses NA
 
-hotel_data[is.na(hotel_data$arrival_date),]
-
 
 #Identificamos datos en blanco
 en_blanco <- function(x){
@@ -50,12 +78,11 @@ en_blanco <- function(x){
     }
   }
 }
-en_blanco(hotel_data)
+en_blanco(hotel_datos)
 #no hay
 
-#DUplicado
-hotel_datos<-unique(hotel_data)
 
+# Técnica utilizada para eliminar o completar los datos faltantes.
 
 #Ingresar datos aleatorios a las columnas lead_time, arrival_date_year, arrival_date_day_of_month
 rand.valor <- function(x){
@@ -75,13 +102,7 @@ random.df <- function(df, cols){
 }
 hotel_datos<-random.df(hotel_datos, c(3, 4, 7))
 
-# * Identificación de datos faltantes (NA).
-summary(hotel_datos[, c('children','babies')])
-# Existen 4 filas sin cantidad de niños y 32 sin cantidad de bebés
-
-
-# * Técnica utilizada para eliminar o completar los datos faltantes.
-
+# Completar datos de children
 # Dado que las familias con niños requieren un poco más de espacio en el cuarto,
 # usaremos la variable tipo de cuarto para comparar
 boxplot(hotel_datos$children ~ hotel_datos$reserved_room_type, ylim  = c(0,3),
@@ -101,12 +122,41 @@ hotel_datos[is.na(hotel_datos$children),'children'] <- sample(c(0,1), replace=TR
 hotel_datos[empty_children_rows,c('reserved_room_type','children')]
 # Respecto a la cantidad de bebés, no se aprecia una categoría
 # que muestre cierta relación con la cantidad de bebés
+
+# Completar datos de babies
 barplot(table(hotel_datos$babies))
 empty_babies_rows <- as.integer(rownames(hotel_datos[is.na(hotel_datos$babies),]))
 # Sin embargo, se aprecia que una mayoría considerable de personas
 # no se hospeda con bebés, por lo cual se completarán los datos con 0
 hotel_datos[is.na(hotel_datos$babies),'babies'] <- 0
 hotel_datos[empty_babies_rows,c('reserved_room_type','babies')]
+
+# Completar datos de adultos
+#verificamos datos atipicos de los adultos
+boxplot(x = hotel_datos$adults)
+table(hotel_datos$adults)
+#Creamos la funcion de la moda
+calc_mode <- function(x){
+  distinct_tabulate <- tabulate(match(x, distinct_values))
+  # Return the value with the highest occurrence
+  distinct_values[which.max(distinct_tabulate)]
+}
+
+
+# Completar datos de arrival_date_week_number
+# arrival_date_week_number : 25 
+empty_weeks.nums <- as.integer(rownames(hotel_datos[is.na(hotel_datos$arrival_date_week_number), ]))
+hotel_datos[empty_weeks.nums,c('arrival_date', 'arrival_date_week_number')]
+empty_weeks.days <- as.POSIXlt(hotel_datos$arrival_date[empty_weeks.nums])
+empty_weeks.weeks <- (empty_weeks.days$yday) %/%7 +1
+hotel_datos$arrival_date_week_number[is.na(hotel_datos$arrival_date_week_number)] = empty_weeks.weeks
+
+
+
+
+
+
+
 
 # * Identificación de datos atípicos (Outliers).
 # * Técnica(s) utilizada(s) para transformar los datos atípicos
@@ -124,48 +174,5 @@ outline_babies_rows <- as.integer(rownames(hotel_datos[hotel_datos$babies >= 9,]
 hotel_datos[outline_babies_rows,'babies'] <- 0
 hotel_datos[outline_babies_rows,c('reserved_room_type','babies')]
 
-#las columnas is_canceled e is_repeated_guest serán convertidas en factor
-hotel_datos$is_canceled <- as.factor(hotel_datos$is_canceled)
-hotel_datos$is_repeated_guest <- as.factor(hotel_datos$is_repeated_guest)
 
-#la columna reservation_status_date será convertida a date
-#verificamos el formato de la fecha es dd/mm/yyyy o mm/dd/yyyy
-head(hotel_datos$reservation_status_date,50)
-#comprobamos que es mm/dd/yyyy
-hotel_datos$reservation_status_date <- as.Date(hotel_datos$reservation_status_date, "%m/%d/%y")
-
-#Concatenaremos las columnas "arrival_date_..." para crear una sola con formato fecha
-#verificamos que los meses tienen formato con nombre del mes, por tanto los transformaremos en número
-
-
-
-hotel_datos$arrival_date <- paste(hotel_datos$arrival_date_year,
-                                 match(substr( hotel_datos$arrival_date_month, 1, 3), month.abb),
-                                 hotel_datos$arrival_date_day_of_month, sep="-")
-hotel_datos$arrival_date <- as.Date(hotel_datos$arrival_date)
-summary(hotel_datos$arrival_date)
-
-
-#volvemos a comprobar la tabla
-str(hotel_datos)
-
-#verificamos datos atipicos de los adultos
-boxplot(x = hotel_datos$adults)
-table(hotel_datos$adults)
-
-#Creamos la funcion de la moda
-calc_mode <- function(x){
-  distinct_tabulate <- tabulate(match(x, distinct_values))
-  # Return the value with the highest occurrence
-  distinct_values[which.max(distinct_tabulate)]
-  
-  
-}
-
-# arrival_date_week_number : 25 
-empty_weeks.nums <- as.integer(rownames(hotel_datos[is.na(hotel_datos$arrival_date_week_number), ]))
-hotel_datos[empty_weeks.nums,c('arrival_date', 'arrival_date_week_number')]
-empty_weeks.days <- as.POSIXlt(hotel_datos$arrival_date[empty_weeks.nums])
-empty_weeks.weeks <- (empty_weeks.days$yday) %/%7 +1
-hotel_datos$arrival_date_week_number[empty_weeks.nums] <- empty_weeks.weeks
 
